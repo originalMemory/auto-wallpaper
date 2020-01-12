792a6e9380326915eaedb03fa6fd401a6fd88824
originalMemory <kdi1994@163.com>
2020-01-12 23:34:35 +0800

    ADD: 对文件名列表排序

--- app/src/main/java/com/example/autowallpaper/helper/FileExtension.kt
+++ app/src/main/java/com/example/autowallpaper/helper/FileExtension.kt
@@ -5,5 +5,5 @@ import java.io.File
 fun File.getImagePathList(): List<String> {
     return listFiles { _, name ->
         name.isImage()
-    }?.map { it.absolutePath } ?: listOf()
+    }?.map { it.absolutePath }?.sorted() ?: listOf()
 }
