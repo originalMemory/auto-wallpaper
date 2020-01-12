23b7ac13d4de16843f9b970ae5e05c44d9ddf46c
originalMemory <kdi1994@163.com>
2020-01-12 17:23:42 +0800

    ADD: 软键盘切换

---
+++ app/src/main/java/com/example/autowallpaper/helper/ContextExtension.kt
@@ -0,0 +1,26 @@
+package com.example.autowallpaper.helper
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Context?.showSoftKeyboard(view: View?) {
+    this ?: return
+    view ?: return
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    inputMethodManager?.showSoftInput(view, 0)
+}
+
+fun Context?.hideSoftKeyboard(view: View?) {
+    this ?: return
+    view ?: return
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun Context?.toggleSoftKeyboard(view: View?) {
+    this ?: return
+    view ?: return
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    inputMethodManager?.toggleSoftInputFromWindow(view.windowToken, 0, InputMethodManager.HIDE_NOT_ALWAYS)
+}
