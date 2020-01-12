3d259c5844b463dd7d0c7865e862884dc2029f61
originalMemory <kdi1994@163.com>
2020-01-12 23:33:38 +0800

    ADD: 夜间休眠及锁屏壁纸裁剪

--- app/src/main/java/com/example/autowallpaper/AutoWallpaperService.kt
+++ app/src/main/java/com/example/autowallpaper/AutoWallpaperService.kt
@@ -3,14 +3,17 @@ package com.example.autowallpaper
 import android.app.Service
 import android.app.WallpaperManager
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Binder
 import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.util.Log
+import com.example.autowallpaper.helper.GlobalApplication
 import com.example.autowallpaper.helper.getImagePathList
 import java.io.File
+import java.util.*
 
 
 class AutoWallpaperService : Service() {
@@ -31,9 +34,19 @@ class AutoWallpaperService : Service() {
     private val handler by lazy { Handler() }
     private val runnable: Runnable by lazy {
         Runnable {
+            val date = Calendar.getInstance()
+            val hour = date.get(Calendar.HOUR_OF_DAY)
+            if (hour < 8) {
+                date.set(Calendar.HOUR_OF_DAY, 8)
+                date.set(Calendar.MINUTE, 0)
+                date.set(Calendar.SECOND, 0)
+                val delayTime = date.timeInMillis - System.currentTimeMillis()
+                handler.postDelayed(runnable, delayTime)
+            }
             val imagePath = imagePaths[currentImageIndex]
             log("index: $currentImageIndex\timageName:${imagePath.substringAfterLast('/')}")
-            setWallpaper(imagePath)
+            setWallpaper(imagePath, true)
+            setWallpaper(imagePaths[imagePaths.size - currentImageIndex - 1], false)
             wallpaperChangeListener?.invoke(currentImageIndex, imagePath)
 
             currentImageIndex = if (currentImageIndex < imagePaths.size - 1) currentImageIndex + 1 else 0
@@ -46,18 +59,44 @@ class AutoWallpaperService : Service() {
         Log.i(TAG, info)
     }
 
-    private fun setWallpaper(imagePath: String) {
+    private fun setWallpaper(imagePath: String, isSystem: Boolean) {
         try {
-            val bitmap = BitmapFactory.decodeFile(imagePath)
+            var bitmap = BitmapFactory.decodeFile(imagePath)
+            if (!isSystem) {
+                log("oldWidth: ${bitmap.width}\toldHeight:${bitmap.height}")
+                val displayMetrics = GlobalApplication.instance.resources.displayMetrics
+                val screenScale = displayMetrics.widthPixels / displayMetrics.heightPixels.toFloat()
+                val newWidth = (bitmap.height * screenScale).toInt()
+                // 只裁剪
+                when {
+                    // 裁剪宽度
+                    newWidth < bitmap.width -> {
+                        val startX = (bitmap.width - newWidth) / 2
+                        bitmap = Bitmap.createBitmap(bitmap, startX, 0, newWidth, bitmap.height)
+                        log("newWidth: $newWidth\tnewHeight:${bitmap.height}")
+                    }
+                    // 裁剪高度
+                    newWidth > bitmap.width -> {
+                        val newHeight = (bitmap.width / screenScale).toInt()
+                        val startY = (bitmap.height - newHeight) / 2
+                        bitmap = Bitmap.createBitmap(bitmap, 0, startY, bitmap.width, newHeight)
+                        log("newWidth: ${bitmap.width}\tnewHeight:$newHeight")
+                    }
+                    else -> {
+                        log("newWidth: ${bitmap.width}\tnewHeight:${bitmap.height}")
+                    }
+                }
+            }
             val wpManager = WallpaperManager.getInstance(this)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 // On Android N and above use the new API to set both the general system wallpaper and
                 // the lock-screen-specific wallpaper
+                val flag = if (isSystem) WallpaperManager.FLAG_SYSTEM else WallpaperManager.FLAG_LOCK
                 wpManager.setBitmap(
                     bitmap,
                     null,
                     true,
-                    WallpaperManager.FLAG_SYSTEM
+                    flag
                 )
             } else {
                 wpManager.setBitmap(bitmap)
