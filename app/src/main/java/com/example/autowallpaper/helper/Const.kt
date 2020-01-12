e5178890ca54d7da680411bc589eeed7bb7b0b51
originalMemory <kdi1994@163.com>
2020-01-12 17:23:19 +0800

    ADD: 通用常量

---
+++ app/src/main/java/com/example/autowallpaper/helper/Const.kt
@@ -0,0 +1,10 @@
+package com.example.autowallpaper.helper
+
+object Const {
+    const val KEY_TIME_INTERVAL = "timeInterval"
+    const val KEY_IMAGE_FOLDER_PATH = "imageFolderPath"
+    const val KEY_CURRENT_IMAGE_INDEX = "currentImageIndex"
+
+    private const val SECOND = 1000L
+    const val MINUTE = 60 * SECOND
+}
